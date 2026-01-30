6da7d4e56f35e9221cde5a54695767a377651606
FlorianKunz-coding <florian@kunzweb.net>
2026-01-30 08:43:44 +0100

    Updated program

--- Program/CandyMachine/src/main.cpp
+++ Program/CandyMachine/src/main.cpp
@@ -129,5 +129,17 @@ void HandleStateMotorActive()
     digitalWrite(M1_OUT_B, LOW);
     digitalWrite(M2_OUT_A, LOW);
     digitalWrite(M2_OUT_B, LOW);
+
+    if (selectedProduct == 1)
+    {
+      digitalWrite(M1_OUT_A, LOW);
+      digitalWrite(M1_OUT_B, LOW);
+    }
+
+    if (selectedProduct == 2)
+    {
+      digitalWrite(M2_OUT_A, LOW);
+      digitalWrite(M2_OUT_B, LOW);
+    }
   }
 }
